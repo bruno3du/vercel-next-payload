31e58d0cfe1ea4298ed7268791ce204082d050f3
bruno3du <bruno.139737@gmail.com>
2023-11-22 23:05:15 -0300

    chore: fixing postgres connection string

--- src/payload/payload.config.ts
+++ src/payload/payload.config.ts
@@ -5,17 +5,24 @@ import { Config } from "./globals/configs";
 import { postgresAdapter } from "@payloadcms/db-postgres";
 import { slateEditor } from "@payloadcms/richtext-slate";
 
+import { drizzle } from "drizzle-orm/node-postgres";
+import { Pool } from "pg";
+
+// or
+
+// or
+
 export default buildConfig({
   db: postgresAdapter({
     migrationDir: path.resolve(__dirname, "./migrations"),
     pool: {
-      connectionString: process.env.DATABASE_URI,
+      connectionString: process.env.DATABASE_URI_URL + "?sslmode=require",
     },
   }),
   editor: slateEditor({}),
   collections: [Post],
   globals: [Config],
-  
+
   typescript: {
     outputFile: path.resolve(__dirname, "../payload-types.ts"),
   },
