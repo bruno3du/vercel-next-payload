3414fbbc5e76d348b772662d1c87193e6274a640
bruno3du <bruno.139737@gmail.com>
2023-11-24 22:14:52 -0300

    fix do fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,23 +4,9 @@ import CreatePost from "@/components/create-post";
 import getPayloadClient from "@/payload/payloadClient";
 
 export default async function Home() {
-  const getPosts = await getPayloadClient();
-
-  const posts = await getPosts.find({
-    collection: "posts",
-  });
-
   return (
     <main className={styles.main}>
-      {posts.docs.map((post) => (
-        <div key={post.id}>
-          <h1>{post.title}</h1>
-          <p>{post.body}</p>
-          <p>{post.updatedAt}</p>
-          <p>{post.createdAt}</p>
-        </div>
-      ))}
-      {/* <ListPost /> */}
+      <ListPost />
       <CreatePost />
     </main>
   );
